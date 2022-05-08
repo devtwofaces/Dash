bb0a8f0d6f15185ab52a03f957b90b20ad70fb61
TwoFaces <dev.twofaces@gmail.com>
2022-05-09 04:22:04 +0530

    Initial Commit.

---
+++ app/src/main/java/com/twofaces/dash/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.twofaces.dash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
